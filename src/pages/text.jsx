0eadb7a9ede229edf16c148b01a36fd0984f07a5
Damindi Samaranayaka <damindisamaranayaka2@gmail.com>
2025-03-25 21:46:51 +0530

    enhance file upload validation in FileUploadTest component

--- src/pages/text.jsx
+++ src/pages/text.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 const key= `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0ZWphcmRvdXV0c3NiY2d2eW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NzE0NzMsImV4cCI6MjA1ODQ0NzQ3M30.EpuWMWV7LQq91vyFlDRZYeMYe5lUYkF2OyrPttAgtew`
 const url= "https://ftejardouutssbcgvyoe.supabase.co"
@@ -12,7 +12,24 @@ export default function FileUploadTest(){
             return
         }
         console.log(file)
+
+        const fileName = file.name
+        const extension = fileName.split(".")[fileName.split(".").length-1]  //file name ek . wlin split krla anthima element ek gnnwa. 
+        if(extension!="jpg" && extension!="jpeg" && extension!="png"){
+            alert("Please select a valid image file")
+            return
+        }
+
+
+
         const supabase= createClient(url , key) //meken psse supabase ekay frontend eky athara connection eky hdenawa me url eky key ekay pass karata passe
+        supabase.storage.from("images").upload(file.name, file,{  //file name eken file ek upload wenn kyl denawa
+            cacheControl: "3600",
+            upsert: false
+        }).then((res)=>{
+            console.log(res)
+            alert("File uploaded successfully")
+        })
     }
     return(
         <div>
